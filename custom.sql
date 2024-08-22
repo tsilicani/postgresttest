94e353eb53d439671d275984f7c1e60fdc5f0eb3
Tobias Silicani <silicani80@gmail.com>
2024-08-22 19:21:42 +0200

    Added custom sql

---
+++ custom.sql
@@ -0,0 +1,26 @@
+CREATE table
+    IF NOT EXISTS "public"."pets" (
+        "id" serial NOT NULL,
+        "name" text NOT NULL,
+        "breed" text NOT NULL,
+        "age" int4 NOT NULL,
+        "owner" text NOT NULL,
+        CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+    );
+
+INSERT INTO
+    public.pets (name, breed, age, owner)
+VALUES
+    ('Buddy', 'Golden Retriever', 3, 'John Doe'),
+    ('Lucy', 'Labrador', 5, 'Jane Smith'),
+    ('Charlie', 'Poodle', 2, 'Alice Johnson'),
+    ('Daisy', 'Boxer', 4, 'Mike Brown'),
+    ('Max', 'Beagle', 6, 'Sarah Connor');
+
+CREATE VIEW
+    pets_view AS
+SELECT
+    name,
+    owner
+FROM
+    public.pets
